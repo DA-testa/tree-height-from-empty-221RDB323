319d178ee552ab5f9fbfe7cdf0f7d909f224ce6a
221RDB323 <125268798+221RDB323@users.noreply.github.com>
2023-03-04 20:39:27 +0000

    recursion fix numero dos

--- tree_height.py
+++ tree_height.py
@@ -3,20 +3,22 @@
 # Danila Sinicins 221RDB323
 
 import sys
-#import threading
+import threading
 import numpy
 
 
 def compute_height(n, parents):
     koks=numpy.zeros(n)
-
+    visited=numpy.zeros(n)
     def height(i):
-        if koks[i]!=0:
+        if koks[i]!=0 or visited[i]==1:
             return koks[i]
         if parents[i]==-1:
             koks[i]=1
+            visited[i]=1
         else:
             koks[i]=height(parents[i])+1
+            visited[i]=1
         return koks[i]
 
     for i in range(n):
@@ -41,13 +43,14 @@ def main():
     if "I" in letterInput:
         n=int(input())
         parents=list(map(int, input().split()))
+
     print(compute_height(n, parents))
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
-#threading.stack_size(2**27)   # new thread will get stack of such size
-#threading.Thread(target=main).start()
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
 main()
 # print(numpy.array([1,2,3]))
