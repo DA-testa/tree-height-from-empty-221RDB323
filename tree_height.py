6dcb31ea5f50fad5bfedd9d9c1d2f1b5f9d3feeb
221RDB323 <125268798+221RDB323@users.noreply.github.com>
2023-03-04 20:20:13 +0000

    input fix numero seis

--- tree_height.py
+++ tree_height.py
@@ -2,8 +2,8 @@
 
 # Danila Sinicins 221RDB323
 
-import sys
-import threading
+#import sys
+#import threading
 import numpy
 
 
@@ -46,8 +46,8 @@ def main():
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+#sys.setrecursionlimit(10**7)  # max depth of recursion
+#threading.stack_size(2**27)   # new thread will get stack of such size
+#threading.Thread(target=main).start()
 main()
 # print(numpy.array([1,2,3]))
